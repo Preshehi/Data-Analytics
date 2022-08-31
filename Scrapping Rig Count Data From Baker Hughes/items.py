742320ca20d69fb1b225e87c586766a51f3cf149
Precious Ehihamen <34788683+Preshehi@users.noreply.github.com>
2022-08-31 16:15:42 +0100

    Update items.py

--- Scrapping Rig Count Data From Baker Hughes/items.py
+++ Scrapping Rig Count Data From Baker Hughes/items.py
@@ -6,11 +6,12 @@
 import scrapy
 from scrapy.loader.processors import MapCompose, TakeFirst
 
+#Remove the extension of the file to capture just the file name
 def remove_extension(value):
     return value.split('.')[0]
 
+#Define an item that will hold the scraped data
 class TestBakerItem(scrapy.Item):
-    # define the fields for your item here like:
     file_urls = scrapy.Field()
     files = scrapy.Field()
     file_name = scrapy.Field(input_processor = MapCompose(remove_extension), output_processor = TakeFirst())
